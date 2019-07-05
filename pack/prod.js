60a5141630b60835a7c261e8cf2ae6054aadceb8
blank <1161709455@qq,com>
2019-07-05 10:42:31 +0800

    操作系统

--- pack/prod.js
+++ pack/prod.js
@@ -1,25 +1,35 @@
 const Bundler = require('parcel-bundler');
 const path = require('path');
+const os = require('os');
 const child = require('child_process');
 const { config } = require('./config');
+
+const type = os.type();
+const parttern = /window/ig;
+const isWindow = parttern.test(type);
 
 // 入口文件路径
 const rootPath = path.resolve(__dirname, '..');
 const file = path.join(rootPath, 'index.html');
 const dist = path.join(rootPath, 'dist');
 
-child.exec('rd /s/q ' + dist, function() {
+const clearUp = isWindow ? 'rd /s/q ' + dist : 'rm -rf ' + dist;
+
+child.exec(clearUp, function() {
     console.log('clear up dist');
 });
-
 
 // 使用提供的入口文件路径和选项初始化 bundler
 const bundler = new Bundler(file, config);
 bundler.bundle();
 
 bundler.on('buildEnd', () => {
-    child.exec('md ' + path.join(dist, 'static'));
-    child.exec('xcopy ' + path.join(rootPath, 'static') + ' /s ' + path.join(dist, 'static'), function() {
+    if (isWindow) {
+        const md = 'md ' + path.join(dist, 'static');
+        child.exec(md);
+    }
+    const copy = isWindow ? 'xcopy ' + path.join(rootPath, 'static') + ' /s ' + path.join(dist, 'static') : 'cp -R' + path.join(rootPath, 'static') + path.join(dist, 'static');
+    child.exec(copy, function() {
         console.log('copy static');
         process.exit();
     });
